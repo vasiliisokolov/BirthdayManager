963e2c9a8756d3101304d81af06efeec4f69ac59
VasiliiSokolov <vasilysokolov@mail.ru>
2023-02-19 17:18:13 +0300

    start

--- BirthdayManager.cpp
+++ BirthdayManager.cpp
@@ -1,11 +1,21 @@
 ﻿#include <iostream>
 #include <ctime>
 #include <map>
+#include <iomanip>
+#include <vector>
+
+struct profile
+{
+    std::string name = "";
+    std::time_t t = std::time(nullptr);
+    std::tm date = *std::localtime(&t);
+};
 
 int main()
 {
+    std::vector<profile> list;
     std::string input = "";
-    std::map<std::time_t, std::string> list;
+   
     std::cout << "Birthday Manger." << std::endl;
     while (input != "end")
     {
@@ -13,11 +23,29 @@ int main()
         std::cin >> input;
         if (input == "end")
         {
-            break;
+            
         }
         else
         {
+            std::cout << "Enter the date of birth: ";
+            profile in;
+            std::cin >> std::get_time(&in.date, "%d/%m/%y");
+            //std::cout << input << std::put_time (&in.date,"%d/%m/%y");
+            list.push_back(in);
 
+        }
+    }
+    std::time_t t = std::time(nullptr);
+    std::tm currentDate = *std::localtime(&t);
+    profile find;
+    std::cout << input << std::put_time(&currentDate, "%d/%m/%y");
+    for (int i = 0; i < list.size(); ++i)
+    {
+        if (list[i].date.tm_year == currentDate.tm_year)
+        {
+            find.name = list[i].name;
+            find.date = list[i].date;
+            
         }
     }
 }
