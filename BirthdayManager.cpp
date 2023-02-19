01feed960a30ab9cfb2cee24b43482a83248d1b4
VasiliiSokolov <vasilysokolov@mail.ru>
2023-02-19 16:02:31 +0300

    Добавьте файлы проекта.

---
+++ BirthdayManager.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <ctime>
+#include <map>
+
+int main()
+{
+    std::string input = "";
+    std::map<std::time_t, std::string> list;
+    std::cout << "Birthday Manger." << std::endl;
+    while (input != "end")
+    {
+        std::cout << "Enter the name: ";
+        std::cin >> input;
+        if (input == "end")
+        {
+            break;
+        }
+        else
+        {
+
+        }
+    }
+}
+
